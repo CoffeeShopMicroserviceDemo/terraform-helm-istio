7898ea45281a9038b7bc7db35027a4265fe93765
Owen Hayes <owen.v.hayes@gmail.com>
2021-09-15 20:59:04 -0700

    Added depends_on

--- data.tf
+++ data.tf
@@ -1,7 +1,9 @@
 data "digitalocean_kubernetes_cluster" "k8s" {
   name = var.do_k8s_name
+  depends_on = [var.do_k8s_name]
 }
 
 data "digitalocean_project" "do_project" {
   id = var.do_project_id
+  depends_on = [var.do_project_id]
 }
